ad2bf66970627215279b9d4f6cdb4a4cf5789d40
Peter-Kibet <peter.kibet@hotmail.com>
2024-07-01 13:44:03 +0300

    configured the playsong function

--- Project3-MusicPlayer/script.js
+++ Project3-MusicPlayer/script.js
@@ -86,6 +86,22 @@ let userData = {
   songCurrentTime: 0,
 };
 
+const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
+
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
+
+  audio.play();
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
