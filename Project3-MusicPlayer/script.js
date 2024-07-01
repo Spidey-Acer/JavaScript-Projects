95316fc6f9949de8fff4a032af5d349a11c8f096
Peter-Kibet <peter.kibet@hotmail.com>
2024-07-01 20:03:17 +0300

    Add a setPlayerDisplay function to display the current playing song

--- Project3-MusicPlayer/script.js
+++ Project3-MusicPlayer/script.js
@@ -99,6 +99,7 @@ const playSong = (id) => {
   playButton.classList.add("playing");
 
   highlightCurrentSong();
+  setPlayerDisplay();
   audio.play();
 };
 
@@ -128,6 +129,16 @@ const playPreviousSong = () => {
 
     playSong(previousSong.id);
   }
+};
+
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
