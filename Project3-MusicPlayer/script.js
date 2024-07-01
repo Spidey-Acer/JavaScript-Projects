7fb888a6b18f5ffb2c469fbb3a816d4a33ce4c8d
Peter-Kibet <peter.kibet@hotmail.com>
2024-07-01 11:01:11 +0300

    Added a sorting function to sort the songs alphabetically

--- Project3-MusicPlayer/script.js
+++ Project3-MusicPlayer/script.js
@@ -104,4 +104,24 @@ const renderSongs = (array) => {
       `;
     })
     .join("");
+
+  playlistSongs.innerHTML = songsHTML;
 };
+
+const sortSongs = () => {
+  userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return userData?.songs;
+};
+
+renderSongs(sortSongs());
