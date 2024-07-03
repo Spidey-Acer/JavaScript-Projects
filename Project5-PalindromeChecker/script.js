4306bb1e0d0b5d527c5ba8a0c04b366acd5defd3
Peter-Kibet <peter.kibet@hotmail.com>
2024-07-03 10:42:38 +0300

    Completed project main functions and it's working

--- Project5-PalindromeChecker/script.js
+++ Project5-PalindromeChecker/script.js
@@ -8,13 +8,9 @@ const removeNonAlphanumerics = (text) => {
   return text.replace(/[\W_]/g, "").toLowerCase();
 };
 
-console.log(removeNonAlphanumerics("ii$ffff^cdf345GGGG"));
-
 //Build the palindrome checker function
 
 const palindromeChecker = (inputTxt) => {
-  // let inputTxt = inputText.value;
-
   let originalTxt = removeNonAlphanumerics(inputTxt);
 
   let reversedText = removeNonAlphanumerics(
@@ -29,13 +25,16 @@ checkButton.addEventListener("click", () => {
 
   let isPalindrome = palindromeChecker(enteredText);
 
-  // console.log("MiCasa")
-  // console.log(enteredText)
   console.log(isPalindrome);
 
   if (isPalindrome === true) {
     result.innerHTML = `<span>${enteredText} is a palindrome</span>`;
   } else {
     result.innerHTML = `<span>${enteredText} is not a palindrome</span>`;
+  }
+
+  if (enteredText.length === 0) {
+    alert("Please input a value");
+    result.innerText = "";
   }
 });
