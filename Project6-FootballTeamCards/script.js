0501c8bb10c7c8b35f29ef569e56978f49807d68
Peter-Kibet <peter.kibet@hotmail.com>
2024-07-04 03:28:46 +0300

    Froze the objects from modification and displayed some of them in the webpage

--- Project6-FootballTeamCards/script.js
+++ Project6-FootballTeamCards/script.js
@@ -170,3 +170,12 @@ const myFavoriteFootballTeam = {
     },
   ],
 };
+
+Object.freeze(myFavoriteFootballTeam);
+const { sport, team, year, players } = myFavoriteFootballTeam;
+const { coachName } = myFavoriteFootballTeam.headCoach;
+
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
