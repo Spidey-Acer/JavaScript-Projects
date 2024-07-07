436309ced6ecc9e4bf2f35402b38e3694efcbfd4
Peter-Kibet <peter.kibet@hotmail.com>
2024-07-08 01:01:59 +0300

    Add a function to delete a task

--- Project7-ToDoapp/script.js
+++ Project7-ToDoapp/script.js
@@ -25,6 +25,7 @@ const addOrUpdateTask = () => {
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   }
+
   updateTaskContainer();
   reset();
 };
@@ -38,11 +39,17 @@ const updateTaskContainer = () => {
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button> 
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
       `;
   });
+};
+
+const deleteTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
 };
 
 const reset = () => {
