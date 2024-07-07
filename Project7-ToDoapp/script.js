11569010ec233e395266f17984e79cff1e25e169
Peter-Kibet <peter.kibet@hotmail.com>
2024-07-07 13:00:33 +0300

    Created an event listener to submit the tasks from the taskObject when clicked

--- Project7-ToDoapp/script.js
+++ Project7-ToDoapp/script.js
@@ -30,4 +30,12 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
 });
