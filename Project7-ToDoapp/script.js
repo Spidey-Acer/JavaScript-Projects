9c8ea5fe596eaf9f3cf8b9a07b33d6d19ac7edc0
Peter-Kibet <peter.kibet@hotmail.com>
2024-07-07 23:27:32 +0300

    Add a reset function to reset the tasks inputs so that new tasks can be entered

--- Project7-ToDoapp/script.js
+++ Project7-ToDoapp/script.js
@@ -13,6 +13,14 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+};
+
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
 );
@@ -54,4 +62,5 @@ taskForm.addEventListener("submit", (e) => {
         </div>
       `;
   });
+  reset();
 });
